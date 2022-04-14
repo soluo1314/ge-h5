5340eb7480997f5c0e0a9c4664dd806cd55f98b4
luo <136269405@qq.com>
2022-04-14 22:56:08 +0800

    feat: 部署H5

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Description:
+ * @Author: lz
+ * @Date: 2022-04-14 22:08:14
+ * @LastEditTime: 2022-04-14 22:10:05
+ * @LastEditors: lz
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
@@ -8,6 +15,7 @@ const createRouter = () =>
   new VueRouter({
     // mode: 'history', // require service support
     scrollBehavior: () => ({ y: 0 }),
+    baseUrl: '/h5',
     routes: basicRoutes,
   })
 
--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: '/h5',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
